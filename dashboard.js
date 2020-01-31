1b15f685136af220ebbf1069f609127d33c46c91
klhexpress <47010729+klhexpress@users.noreply.github.com>
2020-02-01 02:22:34 +0700

    search func

--- dashboard.js
+++ dashboard.js
@@ -12,12 +12,21 @@ async function test() {
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
-    const response2 = await fetch(file);
-    const data2 = await response2.text();
+    //const response2 = await fetch(file);
+    //const data2 = await response2.text();
+    //console.log(data2);
+}
+
+async function search(input){
+    var i = 0;
+    console.log("inside search function" + input);
+    url2 = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${input}&apikey=ISMWAHX9Y5PH9DLP`;
+    console.log("inside search function" + url2);
+    response2 = await fetch(url2);
+    data2 = await response2.json();
     console.log(data2);
 }
 
-
-test().catch(err => {
+/*test().catch(err => {
     console.log("error");
-});
+});*/
