f27dd5238cef414bc451f08ef0e77ca3480e1def
klhexpress <47010729+klhexpress@users.noreply.github.com>
2023-03-01 13:26:02 -0800

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,29 +1,28 @@
 //Khoi Nguyen's Code
 
 async function extractNews() {
-    var url = "http://api.mediastack.com/v1/news?access_key=fa2b4127a6212915b770b9c70a29876a&categories=technology&countries=us";
+    var url = "https://newsapi.org/v2/top-headlines?country=us&category=business&pageSize=100&apiKey=69c05c3e9def4e2aaee0d739b28ee3f1";
 
     var cardContainer = document.getElementById('newsCards');
     var response = await fetch(url)
         .then((response) => response.json())
         .then((responseJSON) => {
 
-            var total = responseJSON["pagination"]["count"];
-            console.log(total);
-            var obj = responseJSON["data"];
+            var total = responseJSON.totalResults;
+            var obj = responseJSON["articles"];
             for (i = 0; i < total; i++) {
 
                 let hyperlinkCard = document.createElement('a');
-                hyperlinkCard.href = obj[i]["url"];
+                hyperlinkCard.href = obj[i].url;
                 hyperlinkCard.target = "_blank";
 
                 let card = document.createElement('div');
                 card.className = 'card shadow';
 
-                if (obj[i]["image"]) {
+                if (obj[i].urlToImage) {
                     let cardpic = document.createElement("img");
                     cardpic.className = "mb-n5 card-img-top";
-                    cardpic.src = obj[i]["image"];
+                    cardpic.src = obj[i].urlToImage;
                     card.appendChild(cardpic);
                 }
 
@@ -32,15 +31,15 @@ async function extractNews() {
 
                 let title = document.createElement('h5');
                 title.className = "card-title";
-                title.innerText = obj[i]["title"];
+                title.innerText = obj[i].title;
 
                 let subtitle = document.createElement("h6");
                 subtitle.className = "card-subtitle text-muted";
-                //subtitle.innerText = (obj[i]["author"] ? obj[i]["author"] : "") + " " + obj[i]["publishedAt"].slice(0, 10);
+                subtitle.innerText = (obj[i].author ? obj[i].author : "") + " " + obj[i].publishedAt.slice(0, 10);
 
                 let description = document.createElement('p');
                 description.className = "card-text text-muted";
-                description.innerText = obj[i]["description"];
+                description.innerText = obj[i].description;
 
                 card.appendChild(cardBody);
                 card.appendChild(title);
